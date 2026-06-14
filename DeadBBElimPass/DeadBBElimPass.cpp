75c89887d133a6c3bdf2ee5040c4071cb4317693
Mina Protic <minaproticc@gmail.com>
2026-06-14 16:46:40 +0200

    Add DeadBBElimPass skeleton

--- DeadBBElimPass/DeadBBElimPass.cpp
+++ DeadBBElimPass/DeadBBElimPass.cpp
@@ -1,1 +1,28 @@
-// Dead Basic Block Elimination LLVM pass implementation will be aded here.
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+namespace {
+
+struct DeadBBElimPass : public FunctionPass {
+    static char ID;
+
+    DeadBBElimPass() : FunctionPass(ID) {}
+
+    bool runOnFunction(Function& F) override {
+        return false;
+    }
+};
+
+}
+
+char DeadBBElimPass::ID = 0;
+
+static RegisterPass<DeadBBElimPass> X(
+    "dead-bb-elim",
+    "Dead Basic Block Elimination Pass",
+    false,
+    false
+);
